729213ff60ddd9fe92c74df32d19c19342bec84a
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-03-16 23:05:07 +1100

    Update script to use latest sdk.

--- Scripts/runUITests.sh
+++ Scripts/runUITests.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
 #checking if xcpretty is available to use
-pretty="xcpretty"
 command -v xcpretty >/dev/null
 if [ $? -eq 1 ]; then
-echo >&2 "xcpretty not found don't use it."
-pretty="&>";
+echo >&2 "xcpretty needed in order to format test results."
 fi
 #run tests using iPhone 6 simulator on iOS 8
 xcodebuild test \
      -workspace WordPress.xcworkspace \
      -scheme UITests \
-     -sdk iphonesimulator8.1 \
+     -sdk iphonesimulator8.2 \
      -destination 'platform=iOS Simulator,name=iPhone 6' | xcpretty -c
